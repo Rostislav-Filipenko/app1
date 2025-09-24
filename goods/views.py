32177bf7432e08a3778d972ae8b3488f0a6dbeac
«Ростислав <rastislav.filipenko@yandex.ru>
2025-09-24 18:58:36 +0300

    the produt filter on the site page and urlencode

--- goods/views.py
+++ goods/views.py
@@ -7,30 +7,36 @@ from goods.models import Products
 
 def catalog(request, category_slug):
 
-    page = request.GET.get('page', 1)
+    page = request.GET.get("page", 1)
 
-    if category_slug == 'all':
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
+
+    if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
+
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
 
-
     context = {
-        'title':'Home - каталог',
-        'goods': current_page,
-        'slug_url': category_slug,
+        "title": "Home - каталог",
+        "goods": current_page,
+        "slug_url": category_slug,
     }
-    return render(request, 'goods/catalog.html', context)
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
 
-    context = {
-        'product': product
-    }
+    context = {"product": product}
 
-    return render(request, 'goods/product.html', context=context)
+    return render(request, "goods/product.html", context=context)
